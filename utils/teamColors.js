e10597540d07584ab362edf5d2d79a7fed938654
Rachit Jain <rgjain04@gmail.com>
2024-03-05 16:27:14 +0530

    team colors for linear gradient

---
+++ utils/teamColors.js
@@ -0,0 +1,36 @@
+const teamColors = {
+  CSE: {
+    topColor: "#0384fc",
+    bottomColor: "#61a7e8"
+  },
+  EE: {
+    topColor: "#4ea9fc",
+    bottomColor: "#a8d1f7"
+  },
+  ECEMETA: {
+    topColor: "#CC9000",
+    bottomColor: "#8c6404"
+  },
+  CIVIL: {
+    topColor: "#f74d52",
+    bottomColor: "#f57a7e"
+  },
+  MECH: {
+    topColor: "#595758",
+    bottomColor: "#787777"
+  },
+  MSc: {
+    topColor: "#595758",
+    bottomColor: "#787777"
+  },
+  MTech: {
+    topColor: "#fa050d",
+    bottomColor: "#fa464c"
+  },
+  PhD: {
+    topColor: "#b846fa",
+    bottomColor: "#d692fc"
+  },
+};
+
+export default teamColors;
